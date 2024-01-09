7113cc8a1289fa2262a442c4db57666c15fd72c7
GautamaJr <gautamajr@hotmail.com>
2024-01-08 23:54:48 -0300

    implementação js da galeria de bicicletas

--- js/script.js
+++ js/script.js
@@ -28,7 +28,6 @@ parametros.forEach(ativarProduto);
 // Perguntas Frequentes
 
 const perguntas = document.querySelectorAll(".perguntas button");
-console.log(perguntas);
 
 function ativarPergunta(event) {
   const pergunta = event.currentTarget;
@@ -45,3 +44,22 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
